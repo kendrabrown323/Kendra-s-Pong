3902f9a819a8e4cc02f7d9a3696db50df02fec39
kendrabrown323 <kendra.brown@untsystem.edu>
2025-05-21 13:31:06 -0500

    music volume raised

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -65,4 +65,4 @@ adventureMusic.player.volume.value = -16;
 export var villageMusic = new soundFile("music.mp3");
 soundArray.push(villageMusic);
 villageMusic.player.loop = true;
-villageMusic.player.volume.value = -22;
+villageMusic.player.volume.value = 6;
